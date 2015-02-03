73e88479c8f0246a65017997fa8cebf2434f211d
Arturo Guzman <arturo@kisurfer.com>
2015-02-02 23:29:17 -0500

    chore(tests): add more tests yay!! :cake: :fireworks:

--- spec/gulpNgConstantSpec.js
+++ spec/gulpNgConstantSpec.js
@@ -1,6 +1,63 @@
 'use strict';
 
 var ngConstant = require('../index');
+var _ = require('lodash');
+
+describe('ngConstant', function () {
+
+    describe('name', function () {
+        it ('creates a modules with "ngConstants" name by default', function (done) {
+            evalNgConstant({name: undefined}, function (_, output) {
+                expect(output).toContain('module("ngConstants"');
+                done();
+            });
+        });
+
+        it ('creates a module with the given name', function (done) {
+            evalNgConstant({name: 'myConstantsModule'}, function (_, output) {
+                expect(output).toContain('module("myConstantsModule"');
+                done();
+            });
+        });
+    });
+
+    describe('deps', function () {
+        it ('creates a new modules by default', function (done) {
+            evalNgConstant({name: 'defaultDeps', deps: undefined}, function (_, output) {
+                expect(output).toContain('module("defaultDeps", [])');
+                done();
+            });
+        });
+
+        it ('adds the given dependencies', function (done) {
+            evalNgConstant({name: 'withDeps', deps: ['ngAnimate']}, function (_, output) {
+                expect(output).toContain('module("withDeps", ["ngAnimate"])');
+                done();
+            });
+        });
+
+        it ('adds constants to an existing module when set to `false`', function (done) {
+            evalNgConstant({name: 'noDeps', deps: false}, function (_, output) {
+               expect(output).toContain('module("noDeps")');
+                done();
+            });
+        });
+    });
+
+    describe('stream', function () {
+        it ('returns an empty open stream when set to `false`', function () {
+            var plugin = ngConstant({stream: false});
+            expect(plugin._readableState.ended).toBe(false);
+            expect(plugin._readableState.length).toBe(0);
+        });
+
+        it ('returns a closed stream with a file when set to `true`', function () {
+            var stream = ngConstant({stream: true});
+            expect(stream._readableState.ended).toBe(true);
+            expect(stream._readableState.length).toBe(1);
+        });
+    });
+});
 
 describe('ngConstant.getConstants', function () {
     it('returns an array with the constants key and json string value', function () {
@@ -46,3 +103,10 @@ describe('ngConstant.getFilePath', function() {
         expect(ngConstant.getFilePath('/foo/bar/foo.js', { dest: 'foo.js' })).toBe('/foo/bar/foo.js');
     });
 });
+
+function evalNgConstant(options, callback) {
+    var defaults = {stream: true, constants: {greeting: 'hello' }};
+    ngConstant(_.merge(defaults, options)).on('data', function (file) {
+        callback(file, file.contents.toString());
+    });
+}
